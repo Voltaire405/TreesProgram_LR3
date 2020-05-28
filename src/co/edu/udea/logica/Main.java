73a05dbd96bf2a68ddf8b5f6c5096856d63093a5
Jose Marin <jmarin@sucomunicacion.com>
2020-05-28 10:21:23 -0500

    first commit and add model

---
+++ src/co/edu/udea/logica/Main.java
@@ -0,0 +1,8 @@
+package co.edu.udea.logica;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/co/edu/udea/logica/Modelo/NodoDoble.java
@@ -0,0 +1,38 @@
+package co.edu.udea.logica.Modelo;
+
+public class NodoDoble {
+    private Integer dato;
+    private  NodoDoble li;
+    private  NodoDoble ld;
+
+    public NodoDoble(Integer dato) {
+        this.setDato(dato);
+        this.setLi(null);
+        this.setLd(null);
+    }
+
+
+    public int getDato() {
+        return dato;
+    }
+
+    public void setDato(Integer dato) {
+        this.dato = dato;
+    }
+
+    public NodoDoble getLi() {
+        return li;
+    }
+
+    public void setLi(NodoDoble li) {
+        this.li = li;
+    }
+
+    public NodoDoble getLd() {
+        return ld;
+    }
+
+    public void setLd(NodoDoble ld) {
+        this.ld = ld;
+    }
+}
